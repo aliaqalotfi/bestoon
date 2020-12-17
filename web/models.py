4e0cbfeb8f23a165990a7fc1eb8774d815e04a58
ali <alilotfi256@gmail.com>
2020-12-17 16:37:08 +0330

    ading expense model

--- web/models.py
+++ web/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+# class Expense(models.Model):
+#
+#     text=models.CharField(_( max_length=250)
+#
+#     date =models.DateTimeField()
+#     amount =models.BigIntegerField()
+#     user= models.ForeignKey(User, on_delete=models.CASCADE)
+class Expense(models.Model):
+    text=models.CharField( max_length=250)
+    time=models.DateTimeField()
+    amount =models.BigIntegerField()
+    user= models.ForeignKey(User, on_delete=models.CASCADE)
